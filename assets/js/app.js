d65e9ba708a3e735d1cacbd98b17d0e3a96b7e6f
smradupan <martinelias630@gmail.com>
2022-02-19 18:47:12 +0100

    error sign added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 const form = document.querySelector('.form-email');
-const input = document.querySelector("#email");
+const input = document.querySelector(".error-msg");
+const errorSign = document.querySelector('.error-icon');
 const buttonSubmit = document.querySelector('.btn-submit');
 
 form.addEventListener('submit', (e) => {
@@ -12,12 +13,15 @@ form.addEventListener('submit', (e) => {
     if(testRegex.test(email)) {
         window.alert("Your email was sent succesfully!");
     } else {
-        input.classList.add("errorShow");
+        input.classList.add("error-show");
+        errorSign.classList.add("error-show");
         buttonSubmit.disabled = true;
+        console.log('error')
     }
 });
 
 input.addEventListener('focus', () => {
-    input.classList.remove('errorShow');
+    input.classList.remove('error-show');
+    errorSign.classList.remove("error-show");
     submitButton.disabled = false;
 });
